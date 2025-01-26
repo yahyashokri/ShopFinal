6fa1ccf7ceb5cfe662ab95aa5e9055e6a6316759
thehsd <hosseinialireza485@gmail.com>
2025-01-27 01:00:10 +0330

    feat: add environment configuration setup
    - Created `.env.example` file to document required environment variables
    - Updated `next.config.js` to include environment variable support
    - Ensured sensitive data is excluded from version control by adding `.env` to `.gitignore`

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: { domains: ['pjereaeszrgxezwznwbw.supabase.co'], },
+env: {
+  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+}
 }
 
 export default nextConfig;
